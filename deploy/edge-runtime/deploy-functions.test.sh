7f65516530e8b8b1eed333766ba7ab121e7296de
luizjuniormedi-ship-it <luizjuniormedi@gmail.com>
2026-08-01 21:52:56 -0300

    test(deploy): cover privileged edge contract audit

--- deploy/edge-runtime/deploy-functions.test.sh
+++ deploy/edge-runtime/deploy-functions.test.sh
@@ -8,6 +8,9 @@ trap_line=""
 release_line=""
 cleanup_found=0
 activation_found=0
+audit_mode_found=0
+nginx_contract_found=0
+rpc_contract_found=0
 while IFS= read -r line; do
   line_number=$((line_number + 1))
   case "$line" in
@@ -15,6 +18,9 @@ while IFS= read -r line; do
     'mkdir "$release"') release_line="$line_number" ;;
     *'rm -rf -- "$release"'*) cleanup_found=1 ;;
     'activated=1') activation_found=1 ;;
+    *'--audit-contract'*) audit_mode_found=1 ;;
+    *'location = /functions/v1/${route}'*) nginx_contract_found=1 ;;
+    *'public.current_context_is_company_admin(uuid)'*) rpc_contract_found=1 ;;
   esac
 done < "$script"
 
@@ -23,5 +29,8 @@ test -n "$release_line"
 test "$trap_line" -lt "$release_line"
 test "$cleanup_found" = "1"
 test "$activation_found" = "1"
+test "$audit_mode_found" = "1"
+test "$nginx_contract_found" = "1"
+test "$rpc_contract_found" = "1"
 
 echo "EDGE_DEPLOY_FAILURE_CLEANUP_CONTRACT_PASS"
